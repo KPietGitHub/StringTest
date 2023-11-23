7ea014b2f7afac6971398811564fb01de4357d8f
g4uuby <kacpergithub27@gmail.com>
2023-11-23 13:26:21 +0100

    skonczone

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner ciag = new Scanner(System.in);
+        System.out.println("Podaj ciąg znaków");
+        String str = ciag.nextLine();
 
-        }
+        System.out.println(str);
+
+
+        int iloscZnakow = str.length();
+
+        System.out.println(iloscZnakow);
+
+       if(iloscZnakow % 2 == 0){
+           String pierwszyZnak = str.substring();
+           String drugiZnak = str.substring();
+       }
+
+
+
+
+       // String drugiZnak  = str.substring();
+
+
+        //String polaczoneZnaki = pierwszyZnak.concat(drugiZnak);
+
+        //System.out.println(polaczoneZnaki);
+
+
+
+
+
+
+
+
+
     }
 }
